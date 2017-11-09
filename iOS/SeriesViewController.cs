3a6defb5919d754be52b8b49d79ae3cbafe10455
hotdiggitydoddo <hotdiggitydoddo@gmail.com>
2017-11-09 14:43:36 -0800

    made e3 wheel larger on launch screen

--- iOS/SeriesViewController.cs
+++ iOS/SeriesViewController.cs
@@ -19,25 +19,18 @@ namespace Exodus3.iOS
             txtAboutSeries.TextContainerInset = UIEdgeInsets.Zero;
             txtAboutSeries.TextContainer.LineFragmentPadding = 0;
 
-            tblViewSermons.Source = new SermonsTableSource(new string[] { "First Sermon", "Second Sermon", "Third Sermon", "Fourth Sermon" });
-            //tblViewSermons.RowHeight = 84;
-            //tblViewSermons.RowHeight = 10;
+            tblViewSermons.Source = new SermonsTableSource(new string[] { "First Sermon", "Second Sermon", "Third Sermon", "Fourth Sermon", "Fifth Sermon", "Sixth Sermon", "Seventh Sermon", "Eighth Sermon", "Ninth Sermon" });
             tblViewSermons.BackgroundColor = UIColor.FromRGB(47f / 255f, 43f / 255f, 33f / 255f);
 
-
-           
-        }
-        public override void ViewDidLayoutSubviews()
-        {
-            base.ViewDidLayoutSubviews();
-            // this line below seems to break the storyboard designer (makes it hang).
-           // txtAboutSeries.ContentOffset = CoreGraphics.CGPoint.Empty;
+            //fix to eliminate the topmost section header and the gap it creates
+            var frame = CoreGraphics.CGRect.Empty;
+            frame.Height = 1;
+            tblViewSermons.TableHeaderView = new UIView(frame);
         }
     }
 
     public class SermonsTableSource : UITableViewSource
     {
-
         string[] TableItems;
         string CellIdentifier = "TableCell";
 
@@ -50,10 +43,12 @@ namespace Exodus3.iOS
         {
             return 1;
         }
+
         public override nint NumberOfSections(UITableView tableView)
         {
             return TableItems.Length;
         }
+
         public override UITableViewCell GetCell(UITableView tableView, NSIndexPath indexPath)
         {
             E3TableViewCell cell = (E3TableViewCell)tableView.DequeueReusableCell(CellIdentifier);
@@ -67,19 +62,15 @@ namespace Exodus3.iOS
             
             return cell;
         }
-
-
     }
 
     public class E3TableViewCell : UITableViewCell
     {
         public E3TableViewCell(UITableViewCellStyle style, string identifier) : base(style, identifier)
         {
-            this.BackgroundColor = UIColor.FromRGB(67f / 255f, 68f / 255f, 59f / 255f);
-            this.TextLabel.TextColor = UIColor.FromRGB(150f / 255f, 154f / 255f, 156f / 255f);
-            this.TextLabel.Font = UIFont.FromName("BrandonGrotesque-Medium", 20f);
+            BackgroundColor = UIColor.FromRGB(67f / 255f, 68f / 255f, 59f / 255f);
+            TextLabel.TextColor = UIColor.FromRGB(150f / 255f, 154f / 255f, 156f / 255f);
+            TextLabel.Font = UIFont.FromName("BrandonGrotesque-Medium", 20f);
         }
-
-      
     }
 }
