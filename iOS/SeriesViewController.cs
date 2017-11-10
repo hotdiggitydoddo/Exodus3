b2b190a77cc87669e85fbe15b90212731067afa2
hotdiggitydoddo <hotdiggitydoddo@gmail.com>
2017-11-09 17:17:33 -0800

    Add navigation ctrls and sermon view wip.

--- iOS/SeriesViewController.cs
+++ iOS/SeriesViewController.cs
@@ -16,16 +16,34 @@ namespace Exodus3.iOS
            
             lblTitle.Font = UIFont.FromName("BrandonGrotesque-Bold", 26f);
             txtAboutSeries.Font = UIFont.FromName("BrandonGrotesque-Medium", 20f);
-            txtAboutSeries.TextContainerInset = UIEdgeInsets.Zero;
-            txtAboutSeries.TextContainer.LineFragmentPadding = 0;
+            //txtAboutSeries.TextContainerInset = UIEdgeInsets.Zero;
+            //txtAboutSeries.TextContainer.LineFragmentPadding = 0;
 
-            tblViewSermons.Source = new SermonsTableSource(new string[] { "First Sermon", "Second Sermon", "Third Sermon", "Fourth Sermon", "Fifth Sermon", "Sixth Sermon", "Seventh Sermon", "Eighth Sermon", "Ninth Sermon" });
+            //title, and table source will be obtained via api call
+            Title = "Judges";
+            lblTitle.Text = "Judges";
+            tblViewSermons.Source = new SermonsTableSource(new string[] { "First Sermon", "Second Sermon", "Third Sermon", "Fourth Sermon", "Fifth Sermon", "Sixth Sermon", "Seventh Sermon", "Eighth Sermon", "Ninth Sermon" }, this);
             tblViewSermons.BackgroundColor = UIColor.FromRGB(47f / 255f, 43f / 255f, 33f / 255f);
 
             //fix to eliminate the topmost section header and the gap it creates
             var frame = CoreGraphics.CGRect.Empty;
             frame.Height = 1;
             tblViewSermons.TableHeaderView = new UIView(frame);
+
+           // NavigationController.NavigationBarHidden = true;
+
+        }
+
+        public override void ViewWillAppear(bool animated)
+        {
+            base.ViewWillAppear(animated);
+            NavigationController.NavigationBarHidden = true;
+        }
+
+        public override void ViewWillDisappear(bool animated)
+        {
+            base.ViewWillDisappear(animated);
+            NavigationController.NavigationBarHidden = false;
         }
     }
 
@@ -34,9 +52,12 @@ namespace Exodus3.iOS
         string[] TableItems;
         string CellIdentifier = "TableCell";
 
-        public SermonsTableSource(string[] items)
+        SeriesViewController owner;
+
+        public SermonsTableSource(string[] items, SeriesViewController owner)
         {
             TableItems = items;
+            this.owner = owner;
         }
 
         public override nint RowsInSection(UITableView tableview, nint section)
@@ -47,6 +68,20 @@ namespace Exodus3.iOS
         public override nint NumberOfSections(UITableView tableView)
         {
             return TableItems.Length;
+        }
+
+        public override void RowSelected(UITableView tableView, NSIndexPath indexPath)
+        {
+            var board = UIStoryboard.FromName("Main", null);
+
+            var sermonCtrl = board.InstantiateViewController("SermonViewController") as SermonViewController;
+            sermonCtrl.Title = TableItems[indexPath.Section];
+            sermonCtrl.SermonName = TableItems[indexPath.Section];
+            sermonCtrl.SeriesName = owner.Title;
+
+            owner.NavigationController.PushViewController(sermonCtrl, true);
+
+            tableView.DeselectRow(indexPath, true);
         }
 
         public override UITableViewCell GetCell(UITableView tableView, NSIndexPath indexPath)
--- iOS/SermonViewController.cs
+++ iOS/SermonViewController.cs
@@ -6,6 +6,9 @@ namespace Exodus3.iOS
 {
     public partial class SermonViewController : UIViewController
     {
+        public string SermonName { get; set; }
+        public string SeriesName { get; set; }
+
         public SermonViewController (IntPtr handle) : base (handle)
         {
         }
