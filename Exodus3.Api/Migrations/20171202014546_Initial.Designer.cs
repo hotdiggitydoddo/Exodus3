c4ee026baf2a933309125e0df9ca483eaf733ac3
hotdiggitydoddo <hotdiggitydoddo@gmail.com>
2017-12-04 07:48:13 -0800

    Update series.

--- Exodus3.Api/Migrations/20171202014546_Initial.Designer.cs
+++ Exodus3.Api/Migrations/20171202014546_Initial.Designer.cs
@@ -12,9 +12,10 @@ using System;
 namespace Exodus3.Api.Migrations
 {
     [DbContext(typeof(E3DbContext))]
-    partial class E3DbContextModelSnapshot : ModelSnapshot
+    [Migration("20171202014546_Initial")]
+    partial class Initial
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
